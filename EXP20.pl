4ec4f69c6706fc96b3b0c21bc5a48f12bd141725
snithiyashree0-commits <snithiyashree0@gmail.com>
2026-06-12 09:29:09 +0530

    Add files via upload

---
+++ EXP20.pl
@@ -0,0 +1,10 @@
+% Planet Database
+
+planet(mercury, 58, 0).
+planet(venus, 108, 0).
+planet(earth, 150, 1).
+planet(mars, 228, 2).
+planet(jupiter, 778, 95).
+planet(saturn, 1434, 146).
+planet(uranus, 2871, 27).
+planet(neptune, 4495, 14).
